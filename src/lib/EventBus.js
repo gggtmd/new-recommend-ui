c2c24eadc608846f2683f9eba4247d2e9c269087
刊wo <1705007045@qq.com>
2024-03-21 23:21:53 +0800

    全局事件总线

---
+++ src/lib/EventBus.js
@@ -0,0 +1,26 @@
+export default class EventBus {
+    constructor() {
+        this.events = {}
+    }
+    on(eventName, fn) {
+        this.events[eventName] = this.events[eventName] || []
+        this.events[eventName].push(fn)
+    }
+    off(evenName, fn) {
+        if(this.events[evenName]){
+            for (let i = 0; i < this.events[evenName].length; i++) {
+                if(this.events[evenName][i] === fn) {
+                    this.events[evenName].splice(i, 1)
+                    break
+                }
+            }
+        }
+    }
+    emit(evenName, ...args) {
+        if(this.events[evenName]) {
+            this.events[evenName].forEach((fn) => {
+                fn(args)
+            })
+        }
+    }
+}
--- src/main.js
+++ src/main.js
@@ -7,14 +7,18 @@ import router from './router'
 import { createPersistedState } from 'pinia-persistedstate-plugin'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import EventBus from "@/lib/EventBus.js";
+
 
 const app = createApp(App)
 const pinia = createPinia()
 const persist = createPersistedState()
+const $bus = new EventBus()
 
 pinia.use(persist)
 app.use(router)
 app.use(pinia)
 app.use(ElementPlus)
+app.provide("$bus", $bus)
 
 app.mount('#app')

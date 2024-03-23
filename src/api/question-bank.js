745af4ee0bc35271f787f9f60c0e765762ec7055
刊wo <1705007045@qq.com>
2024-03-23 23:15:14 +0800

    questionBankListServer API js注解

--- src/api/question-bank.js
+++ src/api/question-bank.js
@@ -1,6 +1,9 @@
 import request from "@/utils/request.js";
 
-
+/**
+ * 题库列表
+ * @returns {Promise<axios.AxiosResponse<any>>}
+ */
 export const questionBankListServer = () => {
     return request.get("/question-bank/list")
 }

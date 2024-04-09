538ca5e864e6f5448221a1bc275d2b777d375d38
刊wo <1705007045@qq.com>
2024-04-09 20:25:52 +0800

    退出登录

--- src/views/my/My.vue
+++ src/views/my/My.vue
@@ -1,9 +1,11 @@
 <script setup>
+import {useRouter} from "vue-router";
+const router = useRouter()
 
 </script>
 
 <template>
-ccc
+  <el-button type="danger" @click="router.push('/login')">退出登录</el-button>
 </template>
 
 <style scoped>

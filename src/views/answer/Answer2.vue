e6d6b0798c5c93dc1c6a13f89ee1db6e7c134294
刊wo <1705007045@qq.com>
2024-04-14 16:27:47 +0800

    知识问答界面修改

--- src/views/answer/Answer2.vue
+++ src/views/answer/Answer2.vue
@@ -1,7 +1,7 @@
 <script setup>
-import {inject, ref} from "vue";
+import {ref} from "vue";
 import { intelligenceSendQuestionServer } from '@/api/intelligence.js'
-import {Position, CloseBold} from '@element-plus/icons-vue'
+import {Position,Loading} from '@element-plus/icons-vue'
 
 const input = ref("")
 const isLoading = ref(false)
@@ -19,54 +19,58 @@ async function getAnswer() {
   answer.value = res.data
 }
 
-//close按钮点击事件
-const closeIconRef = ref(null)
-const emit = defineEmits(["close", "open"])
-const visible = ref(false)
-const $bus = inject("$bus")
-function handleVisible() {
-  if(visible.value) {
-    closeIconRef.value.$el.classList.add("close-icon-end")
-    $bus.emit("closeAnswer")
-  } else {
-    closeIconRef.value.$el.classList.remove("close-icon-end")
-    $bus.emit("openAnswer")
-  }
-  visible.value = !visible.value
+const selectList = ref(['和羹之美，在于合异', '塞翁失马，焉知非福', '咬定青山不放松'])
+const selectAnswer = (item, index) => {
+  input.value = item
+  getAnswer()
 }
-
-defineExpose({
-  handleVisible
-})
 </script>
 
 <template>
   <div class="wrapper">
+    <Transition name="fade">
+      <div class="answer-area pre-answer" v-if="!answer&&!isLoading">
+        <div class="init-answer">
+          <div class="init-title">你可以提出问题了解汉语文化:</div>
+          <div class="init-content">
+            该是一个AI问答系统，你可以提出问题了解汉语文化，例如：和羹之美，在于合异，塞翁失马，焉知非福，咬定青山不放松。
+            你还可以了解其它知识。
+          </div>
+          <div class="init-button">
+            <div class="search-select-item" v-for="(item, index) in selectList" :key="item" @click="selectAnswer(item, index)"><el-button>{{item}}</el-button></div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+    <Transition name="fade">
+      <div class="answer-area pre-answer" v-if="!answer&&isLoading">
+        <div class="loading-statue"><el-icon><Loading /></el-icon></div>
+      </div>
+    </Transition>
     <div class="header">
       <div>知识问答</div>
     </div>
     <el-scrollbar>
-    <Transition name="fade">
-      <div class="answer-area" v-show="!isLoading&&answer">
-        <h1 class="title">{{title}}</h1>
-        <div class="source-wrapper">
-          <img class="source" src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" alt=""></img>
-          <video controls class="source">
-            <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm" type="video/webm" />
-            <a href="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm">WEBM</a>
-          </video>
+      <Transition name="fade">
+        <div class="answer-area" v-show="!isLoading&&answer">
+          <h1 class="title">{{title}}</h1>
+          <div class="source-wrapper">
+            <img class="source" src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" alt=""></img>
+            <video controls class="source">
+              <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm" type="video/webm" />
+              <a href="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm">WEBM</a>
+            </video>
+          </div>
+          <div class="answer">
+            {{answer}}
+          </div>
         </div>
-        <div class="answer">
-          {{answer}}
-        </div>
-      </div>
-    </Transition>
+      </Transition>
     </el-scrollbar>
     <div class="foo">
       <div class="search-select">
-        <div class="search-select-item"><el-button>和羹之美，在于合异</el-button></div>
-        <div class="search-select-item"><el-button>塞翁失马，焉知非福</el-button></div>
-        <div class="search-select-item"><el-button>咬定青山不放松</el-button></div>
+        <div class="search-select-item">猜你想问:</div>
+        <div class="search-select-item" v-for="(item, index) in selectList" :key="item" @click="selectAnswer(item, index)"><el-button>{{item}}</el-button></div>
       </div>
       <div class="search-wrapper">
         <el-input v-model="input" placeholder="请输入问题" @keydown.enter="getAnswer"></el-input>
@@ -85,6 +89,8 @@ defineExpose({
   height: calc(100vh - 60px);
   overflow: hidden;
   position: relative;
+  background-image: radial-gradient(farthest-corner at 40px 40px, rgb(255, 196, 196), rgb(255, 255, 255, 0) 45%),
+                    radial-gradient(farthest-corner at 100% 100%, rgb(245, 255, 177), rgb(255, 255, 255, 0) 55%);
 }
 .header {
   position: absolute;
@@ -116,7 +122,7 @@ defineExpose({
 }
 .source-wrapper {
   width: 100%;
-  height: 350px;
+  height: 300px;
   display: flex;
   align-items: center;
   gap: 20px;
@@ -125,8 +131,8 @@ defineExpose({
   width: 50%;
   height: 100%;
   object-fit: cover;
-  border-radius: 8px;
-  margin-bottom: 10px;
+  border-radius: 10px;
+  margin-bottom: 15px;
 }
 .answer {
   font-size: 1rem;
@@ -161,6 +167,7 @@ defineExpose({
   transform: translateY(40px);
   display: flex;
   justify-content: start;
+  align-items: center;
 }
 .foo:focus-within .search-select {
   opacity: 1;
@@ -168,7 +175,9 @@ defineExpose({
 }
 .search-select-item {
   height: 40px;
-  margin-right: 20px;
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
 }
 .search-select-item .el-button {
   border-radius: 8px;
@@ -236,5 +245,51 @@ defineExpose({
   display: flex;
   align-items: center;
   margin-right: 3px;
+}
+.pre-answer {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.init-answer {
+  width: 600px;
+  height: 300px;
+  border-radius: 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  padding: 15px;
+  background-color: rgba(255, 255, 255, 0.7);
+}
+.init-title {
+  height: 25px;
+  line-height: 25px;
+  font-size: 25px;
+  margin-bottom: 15px;
+}
+.init-content {
+  height: 195px;
+  color: #555;
+}
+.init-button {
+  display: flex;
+  height: 40px;
+}
+.loading-statue {
+  display: flex;
+  align-items: center;
+  font-size: 2rem;
+  color: #555;
+  animation: infinite 1.5s loadingStart linear;
+  transform-origin: center center;
+}
+@keyframes loadingStart {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>

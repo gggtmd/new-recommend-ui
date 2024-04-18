2ed0b3d42a3d46077a3c4ed81b6045cfc6e52269
850540 <zhangwj@leadingtek.com>
2024-04-18 21:28:57 +0800

    接口错误修改

--- src/api/browseRecords.js
+++ src/api/browseRecords.js
@@ -1,7 +1,5 @@
 import request from "@/utils/request.js";
 import {useUserInfoStore} from "@/stores/userInfo.js";
-const userInfoStore = useUserInfoStore()
-const userInfo = userInfoStore.userInfo
 
 /**
  * 浏览记录的新增/修改
@@ -9,6 +7,8 @@ const userInfo = userInfoStore.userInfo
  * @returns {Promise<axios.AxiosResponse<any>>}
  */
 export const browseRecordsSaveServer = (resourceId) => {
+    const userInfoStore = useUserInfoStore()
+    const userInfo = userInfoStore.userInfo
     return request.post("/browse-records/save", {
         resourceId,
         userId: userInfo.userId

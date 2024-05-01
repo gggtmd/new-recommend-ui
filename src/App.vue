1de7b7ea3c023a7d21e2d2ea3925a004d99c495a
刊wo <zhangwj@leadingtek.com>
2024-05-02 04:49:25 +0800

    中文环境配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 </script>
 
 <template>
-  <router-view></router-view>
+  <el-config-provider :locale="zhCn">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 
 <style scoped>
